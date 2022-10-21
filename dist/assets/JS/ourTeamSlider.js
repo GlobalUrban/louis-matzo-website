65aa72fa3579a7db1b2e6954f4640856e240c4b8
Edgardo <edgardo.avalencia@gmail.com>
2022-10-21 14:04:35 -0600

    Our team section finished

--- dist/assets/JS/ourTeamSlider.js
+++ dist/assets/JS/ourTeamSlider.js
@@ -1,16 +1,16 @@
 const slider = document.getElementById("slider");
-const slides = 4;
-const slideDuration = 2000;
+const slides = 3.5;
+const slideDuration = 7000;
 
 let slideCalculation = slides * 100;
 
 window.addEventListener("load", Slider());
 
 function Slider() {
-  let a = 100;
+  let a = 50;
   setInterval(() => {
     slider.style = `margin-left: -${a}%`;
-    a += 100;
+    a += 50;
     console.log(a);
     a = a == slideCalculation ? 0 : a;
   }, slideDuration);
