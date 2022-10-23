79e43ed6385030aced81efce0242b909e64ef0ff
Edgardo <edgardo.avalencia@gmail.com>
2022-10-23 12:32:31 -0600

    Services & Portfolio Section started

--- dist/assets/JS/main.js
+++ dist/assets/JS/main.js
@@ -1,4 +1,3 @@
-
 // Main Slider Assets //
 const buttons = document.getElementsByClassName("button");
 const first = document.getElementById("first");
@@ -6,62 +5,56 @@ const navBottomBar = document.getElementById("bottomBar");
 const map = document.getElementById("map");
 const mapButton = document.getElementById("mapButton");
 
-// Main Slider & Nav Bar// 
+// Main Slider & Nav Bar Animation//
 buttons[0].addEventListener("click", () => {
   first.style = "margin-left: 0%;";
   navBottomBar.style = "margin-left: 0%;";
-  window.scrollBy(0,1000);
+  window.scrollBy(0, 1000);
 });
 
 buttons[1].addEventListener("click", () => {
   first.style = "margin-left: -20%;";
   navBottomBar.style = "margin-left: 21%;";
-  window.scrollBy(0,1000);
+  window.scrollBy(0, 1000);
 });
 
 buttons[2].addEventListener("click", () => {
   first.style = "margin-left: -40%;";
   navBottomBar.style = "margin-left: 41.5%;";
-  window.scrollBy(0,1000);
+  window.scrollBy(0, 1000);
 });
 
 buttons[3].addEventListener("click", () => {
   first.style = "margin-left: -60%;";
   navBottomBar.style = "margin-left: 63%;";
-  window.scrollBy(0,1000);
+  window.scrollBy(0, 1000);
 });
 
 buttons[4].addEventListener("click", () => {
   first.style = "margin-left: -80%;";
   navBottomBar.style = "margin-left: 84%;";
-  window.scrollBy(0,1000);
+  window.scrollBy(0, 1000);
 });
+
 
 // Mobile Slider Assets //
 const mobileNavButton = document.getElementsByClassName("mobileButton");
 const mobileSlide = document.getElementById("mobileSlide");
 const mobileBottomBar = document.getElementById("mobileBottomBar");
 
-
+// Mobile Services Slider //
 mobileNavButton[0].addEventListener("click", () => {
-  console.log("Buenas");
   mobileSlide.style = "margin-left: -0%;";
   mobileBottomBar.style = "margin-left: 0%;";
 });
 
 mobileNavButton[1].addEventListener("click", () => {
-  console.log("Buenas");
   mobileSlide.style = "margin-left: -20%;";
-  mobileBottomBar.style = "margin-left: 20%;";
+  mobileBottomBar.style = "margin-left: 58%;";
 });
 
 
 // Map Expand //
 mapButton.addEventListener("click", () => {
   map.classList.toggle("map-bigger");
-  
 });
-
-
-
-
--- dist/assets/JS/portfolioSlider.js
+++ dist/assets/JS/portfolioSlider.js
@@ -11,3 +11,18 @@ btnRight.addEventListener("click", () => {
   portfolioSlider.style = "margin-left: -100%";
   console.log("se pucho la derecha")
 })
+
+// Mobile Portfolio Slider //
+const mobilePortfolioSlider = document.getElementById("mobilePortfolioSlider");
+const mobileBtnLeft = document.getElementById("mobileBtnLeft");
+const mobileBtnRight = document.getElementById("mobileBtnRight");
+
+mobileBtnLeft.addEventListener("click", () => {
+  mobilePortfolioSlider.style = `margin-left: 0%`;
+  console.log("se pucho la izquierda")
+})
+
+mobileBtnRight.addEventListener("click", () => {
+  mobilePortfolioSlider.style = "margin-left: -100%";
+  console.log("se pucho la derecha")
+})
