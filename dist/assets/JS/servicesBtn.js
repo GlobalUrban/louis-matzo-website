731540068f3bce87e3f33edaff54287fe15fbb47
Edgardo Valencia <edgardo.avalencia@gmail.com>
2022-10-28 16:14:21 -0700

    Services button functionality added to all services

--- dist/assets/JS/servicesBtn.js
+++ dist/assets/JS/servicesBtn.js
@@ -1,7 +1,13 @@
-const serviceBtn = document.getElementById("m-services__btn");
-// const serviceBtn = document.querySelectorAll("m-services__btn");
+// const serviceBtn = document.getElementById("m-services__btn");
+const serviceBtn = document.querySelectorAll(".m-services__btn");
 
-serviceBtn.addEventListener("click", () => {
-   console.log("se puyo el service")
-   serviceBtn.classList.toggle("m-service__btn-pressed");
-  });
+
+serviceBtn.forEach(service => {
+    service.addEventListener("click", () => {
+        console.log("se puyo el service")
+        service.classList.toggle("m-service__btn-pressed");
+       });
+});
+
+
+ 
