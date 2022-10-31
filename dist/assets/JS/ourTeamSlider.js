a725deb83002b0e73c70c2848da1bd690bbe600d
Edgardo Valencia <edgardo.avalencia@gmail.com>
2022-10-31 14:56:26 -0700

    Mobile Slider Bug fixed.

--- dist/assets/JS/ourTeamSlider.js
+++ dist/assets/JS/ourTeamSlider.js
@@ -3,28 +3,42 @@ const slides = 3.5;
 const slideDuration = 7000;
 let slideCalculation = slides * 100;
 
-window.addEventListener("load", Slider());
-
-function Slider() {
-  let a = 50;
-  setInterval(() => {
-    ourTeamSlider.style = `margin-left: -${a}%`;
-    a += 50;
-    a = a == slideCalculation ? 0 : a;
-  }, slideDuration);
-}
-
 // Mobile Slider //
 const slider = document.getElementById("slider");
 const mobileSlides = 4;
 let mobileSlideCalculation = mobileSlides * 100;
 
-window.addEventListener("load", Slider());
-function Slider() {
-  let a = 100;
-  setInterval(() => {
-    slider.style = `margin-left: -${a}%`;
-    a += 100;
-    a = a == mobileSlideCalculation ? 0 : a;
-  }, 7000);
-}
+window.addEventListener("load", () => {
+  function Slider() {
+    let a = 50;
+    setInterval(() => {
+      ourTeamSlider.style = `margin-left: -${a}%`;
+      a += 50;
+      a = a == slideCalculation ? 0 : a;
+    }, 5000);
+  }
+
+  Slider();
+
+  function MobileSlider() {
+    let a = 100;
+    setInterval(() => {
+      slider.style = `margin-left: -${a}%`;
+      a += 100;
+      a = a == mobileSlideCalculation ? 0 : a;
+    }, 7000);
+  }
+
+  MobileSlider();
+});
+
+// window.addEventListener("load", Slider());
+
+// function MobileSlider() {
+//   let a = 100;
+//   setInterval(() => {
+//     slider.style = `margin-left: -${a}%`;
+//     a += 100;
+//     a = a == mobileSlideCalculation ? 0 : a;
+//   }, 7000);
+// }
